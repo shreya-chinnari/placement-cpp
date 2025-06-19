74f2953ea2b11e90da588fea7f2d197e7c099de1
Shreya <shreya16180@gmail.com>
2025-06-19 21:38:41 +0530

    Add implementation of sum calculation using pointers in C++

--- Lec_26_Pointers/10_sum_size.cpp
+++ Lec_26_Pointers/10_sum_size.cpp
@@ -11,10 +11,20 @@ int getSum(int arr[], int size)
 
    // int getSum(int arr[], int size) : here arr is a pointer to the first element of the array,
    // not an array itself. When you pass an array to a function in C++, it decays to a pointer to its first element.
+
+   // benefit:
+   // 1. faster: because we don't need to copy the array
+   // 2. more memory efficient: because we don't need to copy the array
+   // 3. we can pass large arrays without worrying about stack overflow
+   // 4. we can pass arrays of different sizes to the same function
+   // 5. we can use pointer arithmetic to access the elements of the array
+   // 6. we can send "part" of the array to the function, not the whole array ****
+   // 7. we can use pointer notation to access the elements of the array
    int sum = 0;
    for (int i = 0; i < size; i++)
    {
       sum += arr[i]; // Accessing elements using array notation
+      // sum += i[arr]; // both are same
    }
    return sum;
 }
@@ -24,6 +34,8 @@ int main()
    int arr[5] = {10, 20, 30, 40, 50};
    int *p = arr; // Pointer p points to the first element of arr
 
-   cout << "sum: " << getSum(arr, 5) << endl;
+   cout << "sum: " << getSum(arr, 5) << endl;     // Passing the whole array, 150
+   cout << "sum: " << getSum(arr + 3, 2) << endl; // Passing a part of the array, 90
+   // Here, arr + 3 points to the 4th element of the array
    return 0;
 }
