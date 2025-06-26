e648c3f7e3f0202f730c2c542e00316941728700
Shreya <shreya16180@gmail.com>
2025-06-26 17:28:15 +0530

    Add implementations of inline functions and default arguments in separate files

---
+++ Lec_30_Macros_GV_IF_DA/05_inline_functions.cpp
@@ -0,0 +1,1 @@
+// An inline function in C++ is a function where the compiler replaces the function call with the actual function code at compile time — just like a macro, but type - safe and better scoped.
---
+++ Lec_30_Macros_GV_IF_DA/07_def_args.cpp
@@ -0,0 +1,23 @@
+// Default arguments allow you to assign default values to function parameters. If the caller omits those arguments, the compiler uses the default values.
+
+#include <iostream>
+using namespace std;
+
+void greet(string name = "Guest", int age = 18)
+{
+   cout << "Hello, " << name << "! Age: " << age << endl;
+}
+
+int main()
+{
+   greet();            // Uses both default values
+   greet("Shreya");    // Uses default age
+   greet("Rahul", 25); // No defaults used
+   return 0;
+}
+
+/*
+Hello, Guest! Age: 18
+Hello, Shreya! Age: 18
+Hello, Rahul! Age: 25
+*/
