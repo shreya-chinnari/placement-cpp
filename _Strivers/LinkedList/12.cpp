b83dd505bd1acb00c34453e538300b02fa8c1147
Shreya <shreya16180@gmail.com>
2025-07-02 21:07:39 +0530

    Add implementation of doubly linked list conversion from array

---
+++ _Strivers/LinkedList/12.cpp
@@ -0,0 +1,62 @@
+// DOUBLY LL {convert arr to DLL}
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+   int data;
+   Node *next;
+   Node *back;
+
+public:
+   Node(int data1, Node *next1, Node *back1)
+   {
+      data = data1;
+      next = next1;
+      back = back1;
+   }
+
+public:
+   Node(int data1)
+   {
+      data = data1;
+      next = nullptr;
+      back = nullptr;
+   }
+};
+
+Node *convertArr_to_DLL(vector<int> &arr)
+{
+   Node *head = new Node(arr[0]);
+   Node *prev = head; // copy
+
+   for (int i = 1; i < arr.size(); i++)
+   {
+      Node *temp = new Node(arr[i], nullptr, prev);
+      prev->next = temp;
+      prev = temp;
+      // prev = prev->next; {{same thing}}
+   }
+   return head;
+}
+
+void print(Node *head)
+{
+   cout << "LL ->   ";
+   while (head != NULL)
+   {
+      cout << head->data << "  ";
+      head = head->next;
+   }
+}
+
+int main()
+{
+   vector<int> arr = {1, 2, 3, 4, 5};
+   Node *head = convertArr_to_DLL(arr);
+   print(head);
+   return 0;
+}
